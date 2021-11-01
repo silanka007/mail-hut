df83a0ce51c0eaf0428298d25283900b61b8390d
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-11-01 20:49:08 +0100

    deployed app to heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.send("hello world");
+  return res.send("app successfully deployed to heroku!!!");
 });
 
 module.exports = app;
