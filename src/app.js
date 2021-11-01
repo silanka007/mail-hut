d60765327098394fbdeb225a41e2e9f3e8d287c2
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-11-01 21:07:07 +0100

    connected frontend to backend

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "../public/")));
 
-app.get("/", (req, res) => {
-  return res.send("app successfully deployed to heroku!!!");
+app.get("/*", (req, res) => {
+  return res.sendFile(path.resolve(__dirname, "../public/index.html"));
 });
 
 module.exports = app;
