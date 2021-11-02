010f8635e5f9c745ea9965df737ac9c0ad24409c
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-11-02 11:12:54 +0100

    implemented authentication with google oauth

--- src/app.js
+++ src/app.js
@@ -2,13 +2,35 @@ const express = require("express");
 const path = require("path");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const { googleClientID, googleClientSecret } = require("./config/keys");
 
 const app = express();
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "../public/")));
 
-// api routes
+// passport initialization
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("from passport", { profile }, profile.emails);
+    }
+  )
+);
 
+// api routes
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 app.get("/*", (req, res) => {
   return res.sendFile(path.resolve(__dirname, "../public/index.html"));
