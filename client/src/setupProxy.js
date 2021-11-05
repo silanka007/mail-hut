bc07ef16b95ff3c9bdfb784fb908dc9fe304916d
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-11-05 23:11:24 +0100

    updated setupProxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -7,6 +7,11 @@ module.exports = function (app) {
     })
   );
   app.use(
+    createProxyMiddleware("/v1/auth/*", {
+      target: "http://localhost:5000",
+    })
+  );
+  app.use(
     createProxyMiddleware("/v1/*", {
       target: "http://localhost:5000",
     })
