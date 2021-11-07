07c76333b6b5fdace1a2659c8287ce25edf7e4a1
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-11-07 19:18:04 +0100

    created survey router

--- src/routes/v1/api.js
+++ src/routes/v1/api.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const authRouter = require("./auth");
+const surveysRouter = require("./surveys");
 const usersRouter = require("./users");
 
 const apiV1Router = express.Router();
 
 apiV1Router.use("/auth", authRouter);
 apiV1Router.use("/users", usersRouter);
+apiV1Router.use("/surveys", surveysRouter)
 
 module.exports = apiV1Router;
---
+++ src/routes/v1/surveys.js
@@ -0,0 +1,6 @@
+const express = require("express")
+
+const surveysRouter = express.Router()
+
+
+module.exports = surveysRouter;
