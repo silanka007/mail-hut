7077a0c6d1b941974cfda5fa74db5ba13dd7f54d
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-11-07 19:16:16 +0100

    created survey model

---
+++ src/models/Survey.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const SurveySchema = mongoose.Schema({
+  title: String,
+  subject: String,
+  body: String,
+  recipients: [{
+    email: String,
+    responded: {
+      type: Boolean,
+      default: false
+    }
+  }],
+  _user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users"
+  },
+  createdDate: Date,
+  lastResponseDate: Date
+})
+
+module.exports = mongoose.Model("surveys", SurveySchema)
